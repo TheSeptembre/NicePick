ec70e98bc66d47cec422c93fbac2b91a8e5086cf
Bastien Chanvillard <39124340+Wishicey@users.noreply.github.com>
2019-02-28 16:59:09 +0100

    accueil.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Nice Pick</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css"
+    </head>
+    <body>
+        <div id="header">
+            <a href="index.php">
+                <img id="logo" src="Ressources/logo.png" alt="NicePick">
+            </a>
+            <a class="bouton" id="coach" href="ConnexionCoach.php">Connexion Coach</a>
+            <a class="bouton" id="joueur" href="ConnexionJoueur.php">Connexion Joueur</a>
+            <a class="bouton" id="pro" href="DecouvrirPro.php">PRO</a>
+            <a class="bouton" id="fun" href="DecouvrirFun.php">FUN</a>
+        </div>
+        <div id="nav">
+
+        </div>
+        <div id="acceuil">
+
+        </div>
+    </body>
+</html>
