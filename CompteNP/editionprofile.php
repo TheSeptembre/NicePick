ae4e767e7ddd72f5592a15f47c15f21a755909b1
Yazh <yann.mathon@epsi.fr>
2019-04-18 11:53:51 +0200

    Update editionprofile.php

--- CompteNP/editionprofile.php
+++ CompteNP/editionprofile.php
@@ -4,37 +4,32 @@ session_start();
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=projet_open;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION));
 
 if(isset($_SESSION['idjoueur'])) {
-   $requser = $bdd->prepare("SELECT * FROM etudiant WHERE idjoueur = ?");
+   $requser = $bdd->prepare("SELECT * FROM joueur WHERE idjoueur = ?");
    $requser->execute(array($_SESSION['idjoueur']));
    $user = $requser->fetch();
    if(isset($_POST['newpseudo']) AND !empty($_POST['newpseudo']) AND $_POST['newpseudo'] != $user['pseudo']) {
       $newpseudo = htmlspecialchars($_POST['newpseudo']);
-      $insertpseudo = $bdd->prepare("UPDATE etudiant SET pseudo = ? WHERE idjoueur = ?");
+      $insertpseudo = $bdd->prepare("UPDATE joueur SET pseudo = ? WHERE idjoueur = ?");
       $insertpseudo->execute(array($newpseudo, $_SESSION['idjoueur']));
       if ($pseudoerror == 0) {     header('Location: profil.php?idjoueur='.$_SESSION['idjoueur']);
 
    }
-   if(isset($_POST['newnom']) AND !empty($_POST['newnom']) AND $_POST['newnom'] != $user['nom']) {
-      $newnom = htmlspecialchars($_POST['newnom']);
-      $insertnom = $bdd->prepare("UPDATE etudiant SET nom = ? WHERE idjoueur = ?");
-      $insertnom->execute(array($newnom, $_SESSION['idjoueur']));
-   }
-   if(isset($_POST['newprenom']) AND !empty($_POST['newprenom']) AND $_POST['newprenom'] != $user['prenom']) {
-      $newprenom = htmlspecialchars($_POST['newprenom']);
-      $insertprenom = $bdd->prepare("UPDATE etudiant SET prenom = ? WHERE idjoueur = ?");
-      $insertprenom->execute(array($newprenom, $_SESSION['idjoueur']));
+   if(isset($_POST['newposte']) AND !empty($_POST['newposte']) AND $_POST['newposte'] != $user['poste']) {
+      $newposte = htmlspecialchars($_POST['newposte']);
+      $insertposteposte = $bdd->prepare("UPDATE joueur SET poste = ? WHERE idjoueur = ?");
+      $insertposte->execute(array($newposte, $_SESSION['idjoueur']));
 
    }
-   if(isset($_POST['newage']) AND !empty($_POST['newage']) AND $_POST['newage'] != $user['age']) {
-      $newage = htmlspecialchars($_POST['newage']);
-      $insertage = $bdd->prepare("UPDATE etudiant SET datenaiss = ? WHERE idjoueur = ?");
-      $insertage->execute(array($newage, $_SESSION['idjoueur']));
+   if(isset($_POST['newrang']) AND !empty($_POST['newrang']) AND $_POST['newrang'] != $user['rang']) {
+      $newrang = htmlspecialchars($_POST['newrang']);
+      $insertrang = $bdd->prepare("UPDATE joueur SET datenaiss = ? WHERE idjoueur = ?");
+      $insertrang->execute(array($newrang, $_SESSION['idjoueur']));
    }
 
    if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['mail']) {
      if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
       $newmail = htmlspecialchars($_POST['newmail']);
-      $insertmail = $bdd->prepare("UPDATE etudiant SET mail = ? WHERE idjoueur = ?");
+      $insertmail = $bdd->prepare("UPDATE joueur SET mail = ? WHERE idjoueur = ?");
       $insertmail->execute(array($newmail, $_SESSION['idjoueur']));
       if ($mailerror == 0) {    header('Location: profil.php?idjoueur='.$_SESSION['idjoueur']);
    }
@@ -42,7 +37,7 @@ if(isset($_SESSION['idjoueur'])) {
       $mdp1 = sha1($_POST['newmdp1']);
       $mdp2 = sha1($_POST['newmdp2']);
       if($mdp1 == $mdp2) {
-         $insertmdp = $bdd->prepare("UPDATE etudiant SET motdepasse = ? WHERE idjoueur = ?");
+         $insertmdp = $bdd->prepare("UPDATE joueur SET motdepasse = ? WHERE idjoueur = ?");
          $insertmdp->execute(array($mdp1, $_SESSION['idjoueur']));
       } else {
          $msg = "Vos deux mdp ne correspondent pas !";
@@ -55,7 +50,7 @@ if(isset($_SESSION['idjoueur'])) {
   }
 }
 }
-}
+}joueur
 
 ?>
 <html>
@@ -101,12 +96,10 @@ if(isset($_SESSION['idjoueur'])) {
         <form method="POST" action="" enctype="multipart/form-data">
            <label>Pseudo :</label>
            <input type="text" name="newpseudo" placeholder="Pseudo" value="<?php echo $user['pseudo']; ?>" /><br /><br />
-           <label>Nom :</label>
-           <input type="text" name="newnom" placeholder="Nom" value="<?php echo $user['nom']; ?>" /><br /><br />
-           <label>Prenom :</label>
-           <input type="text" name="newprenom" placeholder="Prenom" value="<?php echo $user['prenom']; ?>" /><br /><br />
-           <label>Age :</label>
-           <input type="text" name="newage" placeholder="Age" value="<?php echo $user['age']; ?>" /><br /><br />
+           <label>Poste :</label>
+           <input type="text" name="newposte" placeholder="poste" value="<?php echo $user['poste']; ?>" /><br /><br />
+           <label>Rang :</label>
+           <input type="text" name="newrang" placeholder="rang" value="<?php echo $user['rang']; ?>" /><br /><br />
            <label>Mail :</label>
            <input type="text" name="newmail" placeholder="Mail" value="<?php echo $user['mail']; ?>" /><br /><br />
            <label>Mot de passe :</label>
